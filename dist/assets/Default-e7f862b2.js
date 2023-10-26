e68be2540ae4daa7a6c613c0ac6e600897af3183
VBoudreau55 <vancebou55@gmail.com>
2023-10-26 00:52:45 -0400

    adding dist subtree

---
+++ dist/assets/Default-e7f862b2.js
@@ -0,0 +1,1 @@
+import{r as s,o as a,c as n,w as c,a as e,V as p,b as u}from"./index-a5267aa1.js";const l={__name:"AppBar",setup(t){return(_,r)=>null}},m={__name:"View",setup(t){return(_,r)=>{const o=s("router-view");return a(),n(p,null,{default:c(()=>[e(o)]),_:1})}}},f={__name:"Default",setup(t){return(_,r)=>(a(),n(u,null,{default:c(()=>[e(l),e(m)]),_:1}))}};export{f as default};
